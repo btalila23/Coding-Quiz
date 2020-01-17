e282595c8e3b0615e6c6a135091167d80bf5ee9c
Talila Burka <btalila@yahoo.com>
2020-01-16 23:43:12 -0600

    Listed global variables and laid out questions and answers

---
+++ questions.js
@@ -0,0 +1,60 @@
+var sel
+var ctdown
+var currentQC
+var score
+var database = [{
+        q: 'Commonly used data types Do Not include:',
+        c: {
+            1: 'Strings',
+            2: 'Booleans',
+            3: 'Alerts',
+            4: 'Numbers'
+        },
+        a: 3
+    },
+
+    {
+        q: 'The condition in an if/else statement is enclosed with ______.',
+        c: {
+            1: 'Quotes',
+            2: 'Curly brackets',
+            3: 'Parentheses',
+            4: 'Square brackets'
+        },
+        a: 3
+    },
+
+    {
+        q: 'Arrays in JavaScript can be used to store ______.',
+        c: {
+            1: 'Numbers and strings',
+            2: 'Other arrays',
+            3: 'Booleans',
+            4: 'All of the above'
+        },
+        a: 4
+    },
+
+    {
+        q: 'String values must be enclosed within ______ when being assigned to variables.',
+        c: {
+            1: 'Quotes',
+            2: 'Curly brackets',
+            3: 'Commas',
+            4: 'Parentheses'
+        },
+        a: 1
+    },
+
+    {
+        q: 'A very useful tool used during development and debugging for printing content to the debugger is?',
+        c: {
+            1: 'JavaScript',
+            2: 'Terminal/bash',
+            3: 'For loops',
+            4: 'Console.log'
+        },
+        a: 4
+    }
+]
+
