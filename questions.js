4d2d2b8a110a30e137d2751528d6eb38c590fca3
Talila Burka <btalila@yahoo.com>
2020-01-16 23:48:06 -0600

    Added event listener, functions and query selector

--- questions.js
+++ questions.js
@@ -58,3 +58,91 @@ var database = [{
     }
 ]
 
+function main() {
+    makeQC(database)
+    ctdown = 7500
+    document.getElementById('time').textContent = Math.floor(ctdown / 100)
+    score = 0
+    setInterval(function() {
+        document.getElementById('time').textContent = Math.floor(ctdown / 100)
+        if (ctdown > 0) {
+            ctdown--
+        } else {
+            jump2score()
+        }
+    }, 10)
+}
+
+function makeQC(obj) {
+    console.log('dmsg: makeQC')
+    sel = generateSelector(obj)
+    loadQC(obj, sel)
+    document.getElementById('score').textContent = score
+}
+
+function generateSelector(obj) {
+    console.log('dmsg: generateSelector')
+    var n = obj.length
+    return Math.floor(
+        (window.crypto.getRandomValues(new Uint32Array(2))[0] / 0xffffffff) * n
+    )
+}
+
+function loadQC(obj, index) {
+    console.log('dmsg: loadQC')
+    renderQC(obj[index])
+    currentQC = obj.splice(index, 1)
+}
+
+function renderQC(obj) {
+    console.log('dmsg: renderQC')
+    console.dir(Object.keys(obj))
+    console.log(obj.q)
+    console.dir(document.getElementsByTagName('h3'))
+    document.getElementById('question').textContent = obj.q
+    console.dir(document.getElementsByTagName('h3'))
+    document.getElementById('choice').innerHTML = ''
+    for (var key in obj.c) {
+        var cDiv = document.createElement('div')
+        var cBtn = document.createElement('button')
+        cBtn.classList.add('btn', 'btn-primary', 'v-left')
+        cBtn.id = key
+        console.log(key)
+        console.dir(obj.c[key])
+        cBtn.textContent = obj.c[key]
+        document.getElementById('choice').appendChild(cDiv)
+        cDiv.appendChild(cBtn)
+        console.dir(cBtn)
+    }
+}
+
+function jump2score() {
+    score += ctdown / 5
+    ctdown = 0
+    window.localStorage.setItem('score', score)
+    console.log(window.localStorage.getItem('score'))
+    document.location.href = 'highscore.html'
+}
+
+main()
+document.querySelector('#choice').addEventListener('click', function(e) {
+    console.log('dmsg: chka')
+    var userSel = Number(e.target.id)
+    if (userSel === currentQC[0].a) {
+        score += 300
+        document.getElementById('score').textContent = score
+        document.querySelector('.answer').textContent = 'Correct!'
+        if (database.length > 0) {
+            makeQC(database)
+        } else {
+            jump2score()
+        }
+    } else {
+        document.querySelector('.answer').textContent = 'Wrong!'
+        if (ctdown > 1500) {
+            ctdown -= 1500
+        } else {
+            ctdown = 0
+        }
+    }
+})
